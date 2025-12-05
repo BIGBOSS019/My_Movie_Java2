a4d009f4c9f7111d2c17993d5d00cb34d1cde1df
BIGBOSS019 <camil.aliyev838@gmail.com>
2025-12-05 20:12:22 +0400

    Movie

---
+++ My-Movie/src/main/java/com/movie/dea/controller/MovieController.java
@@ -0,0 +1,20 @@
+package com.movie.dea.controller;
+
+import com.movie.dea.servise.MovieService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+
+}
+
+
---
+++ My-Movie/src/main/java/com/movie/dea/entity/Movie.java
@@ -0,0 +1,43 @@
+package com.movie.dea.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String genre;
+
+    public Movie(Integer id, String title, String genre) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
