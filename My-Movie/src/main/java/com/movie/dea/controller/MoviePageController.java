93d26428a93a6fcd6db7a65b19d96b85e0cd16d7
BIGBOSS019 <camil.aliyev838@gmail.com>
2026-01-13 20:17:13 +0400

    Movie.... DZ

--- My-Movie/src/main/java/com/movie/dea/controller/MoviePageController.java
+++ My-Movie/src/main/java/com/movie/dea/controller/MoviePageController.java
@@ -4,6 +4,7 @@ import com.movie.dea.entity.Movie;
 import com.movie.dea.servise.MovieService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -26,6 +27,32 @@ public class MoviePageController {
             model.addAttribute("movies", movieService.getAllMovie());
             return "movies/list";
         }
+
+    // формат добавления
+    @GetMapping("/new")
+    public String form(Model model) {
+        model.addAttribute("movie", new Movie());
+        return "movies/new";
+    }
+
+    @PostMapping
+    public String save(@Valid @ModelAttribute Movie movie,
+                       BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "movies/new";
+        }
+
+        movieService.createMovie(movie);
+        return "redirect:/movies";
+    }
+
+    // формат обновлении
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("movie", movieService.getMovie(id));
+        return "movies/edit";
+    }
 
 //    @GetMapping("/all")
 //    public List<Movie> getMovies(){
